9d630e376c8be70f6932c69b6901468a3fc830fd
tomotsune <tomotsune@foxmail.com>
2020-09-22 14:20:19 +0800

    bate1.2

--- link.h
+++ link.h
@@ -10,38 +10,30 @@ struct Node
     explicit Node(T x) :val(x), next(nullptr) {}
 };
 template<typename T>
-class DoubleLink
+class Link
 {
 private:
     Node<T>* head;
     int length{};
 public:
-    DoubleLink();
-    void insertHead(T var);
+    Link();
     void insert(T val, const int& pos);
     void remove(T val);
     int getLength();
-    void reverse();
     int find(T val);
     void print();
-    ~DoubleLink();
+    ~Link();
 };
 
 template<typename T>
-inline DoubleLink<T>::DoubleLink()
+inline Link<T>::Link()
 {
     head = nullptr;
     length == 0;
 }
 
 template<typename T>
-inline void DoubleLink<T>::insertHead(T var)
-{
-    insert(var, 0);
-}
-
-template<typename T>
-inline void DoubleLink<T>::insert(T val, const int& pos)
+inline void Link<T>::insert(T val, const int& pos)
 {
     if (pos < 0) {
         cout << "pos must be greater than zero" << endl;
@@ -68,14 +60,11 @@ inline void DoubleLink<T>::insert(T val, const int& pos)
     //交接后续结点.
     node->next = temp->next;
     temp->next = node;
-    //连接前面的结点.
-    node->prior=temp->prior;
-    temp->prior=node;
     ++length;
 }
 
 template<typename T>
-inline void DoubleLink<T>::remove(T val)
+inline void Link<T>::remove(T val)
 {
     int pos = find(val);
     if (-1 == pos) {
@@ -93,39 +82,20 @@ inline void DoubleLink<T>::remove(T val)
         temp = temp->next;
     }
     Node<T>* removeNode{ temp->next };
-    temp->next = temp->next->next;
-    //交接前面结点.
-    temp->prior = temp->prior->prior;
+    temp->next == temp->next->next;
     delete removeNode;
     --length;
 }
 
 template<typename T>
-inline int DoubleLink<T>::getLength()
+inline int Link<T>::getLength()
 {
     return length;
 }
 
-template<typename T>
-inline void DoubleLink<T>::reverse()
-{
-    if (head == nullptr) {
-        return;
-    }
-    Node<T>* curNode{ head }, * nextNode{ head->next }, * temp;
-    while (nextNode!=nullptr)
-    {
-        temp = nextNode->next;
-        nextNode->next = curNode;
-        curNode = nextNode;
-        nextNode = temp;
-    }
-    head->next = nullptr ;
-    head = curNode;
-}
 
 template<typename T>
-inline int DoubleLink<T>::find(T val)
+inline int Link<T>::find(T val)
 {
     Node<T>* temp = head;
     int index{};
@@ -140,7 +110,7 @@ inline int DoubleLink<T>::find(T val)
 }
 
 template<typename T>
-inline void DoubleLink<T>::print()
+inline void Link<T>::print()
 {
     if (head == nullptr) {
         cout << "List is empty" << endl;
@@ -156,7 +126,7 @@ inline void DoubleLink<T>::print()
 }
 
 template<typename T>
-inline DoubleLink<T>::~DoubleLink()
+inline Link<T>::~Link()
 {
     Node<T>* temp;
     for (int  i = 0; i < length; i++)
